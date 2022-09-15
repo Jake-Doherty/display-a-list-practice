cbd4a5c901c19c3cebfa2d2d79a7545893c08542
Jacob Doherty <doherty.jacob.p@gmail.com>
2022-09-15 11:50:27 -0700

    add export candies array variable to candies-data

---
+++ candies-data.js
@@ -0,0 +1,17 @@
+export const candies = [
+    {
+        name: 'Snickers',
+        flavor: 'Chocolatey peanuts and nougat',
+        type: 'candybar',
+    },
+    {
+        name: 'Reeses',
+        flavor: 'Milk chocolate',
+        type: 'peanut butter cup',
+    },
+    {
+        name: 'Nerds',
+        flavor: 'Purple and Pinkberry',
+        type: 'Sour hard candy',
+    },
+];
